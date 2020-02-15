69ec8c293a52a3b219457b64ce575b2446c72f06
928687514@qq.com <928687514@qq.com>
2020-02-15 19:24:23 +0800

    color

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(153, 227);
+            this.button1.Location = new System.Drawing.Point(130, 213);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
