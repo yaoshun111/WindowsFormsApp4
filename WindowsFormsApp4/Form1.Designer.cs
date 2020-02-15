8a61a013410a74bf9eaecfda3132ffc36495d7c3
928687514@qq.com <928687514@qq.com>
2020-02-15 20:07:50 +0800

    kkk

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -56,7 +56,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(46, 426);
+            this.button2.Location = new System.Drawing.Point(539, 457);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
