b8c67a908b1b2320c969df90c96a0ce12e1fa3cf
yaoshun <yaoshun@DESKTOP-FSP7JBC>
2020-02-15 19:44:52 +0800

    lo21

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -56,7 +56,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(153, 256);
+            this.button2.Location = new System.Drawing.Point(12, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
